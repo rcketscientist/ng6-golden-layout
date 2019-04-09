2da4747aac059e41d462f4eb786140dced22832b
Anthony Mandra <anthony.mandra@keysight.com>
2019-04-09 09:58:27 -0400

    registerComponent

--- projects/ee-golden-layout/src/lib/golden-layout.service.ts
+++ projects/ee-golden-layout/src/lib/golden-layout.service.ts
@@ -1,15 +1,13 @@
 import { Inject, Injectable, Type, Optional } from '@angular/core';
-import { Subject } from 'rxjs';
 import * as GoldenLayout from 'golden-layout';
 import { GoldenLayoutConfiguration, ComponentConfiguration } from './config';
-import { GoldenLayoutStateStore, StateStore, LocalStorageStateStore } from './state';
+import { GoldenLayoutStateStore, StateStore } from './state';
+import { GoldenLayoutComponent } from './golden-layout.component';
 
 /**
  * golden-layout component initialization callback type.
  */
-export interface ComponentInitCallback extends Function {
-  (container: GoldenLayout.Container, componentState: any): void;
-}
+export type ComponentInitCallback = (container: GoldenLayout.Container, componentState: any) => void;
 
 export interface ComponentInitCallbackFactory {
   createComponentInitCallback(component: Type<any>): ComponentInitCallback;
@@ -39,14 +37,14 @@ export class GoldenLayoutService {
   private _saveState(goldenLayout: GoldenLayout): void {
     if (this.stateStore && goldenLayout.isInitialised) {
       try {
-        this.stateStore.writeState(goldenLayout.toConfig());
-      } catch(ex) {
+        this.stateStore.writeState(goldenLayout.toConfig());    // TODO: efficient? http://golden-layout.com/tutorials/saving-state.html
+      } catch (ex) {
         // Workaround for https://github.com/deepstreamIO/golden-layout/issues/192
       }
     }
   }
 
-  public getState(): Promise<any>{
+  public getState(): Promise<any> {
     if (this.stateStore) {
       return this.stateStore.loadState().catch(() => {
         return this.config.defaultLayout;
@@ -56,10 +54,39 @@ export class GoldenLayoutService {
     return Promise.resolve(this.config.defaultLayout);
   }
 
+  public loadState(config: GoldenLayoutConfiguration) {
+    this._layout.config = config.defaultLayout;
+    // FIXME: We have not addressed the component registry!  Honestly, this might not work anyway.
+    // again we want to resolve components in the service not the component
+    // this.config.components.forEach((componentConfig: ComponentConfiguration) => {
+    //   const componentInitCallback = componentInitCallbackFactory.createComponentInitCallback(componentConfig.component);
+    //   goldenLayout.registerComponent(componentConfig.componentName, componentInitCallback);
+    // });
+    this._layout.init();
+  }
+
   public getRegisteredComponents(): ComponentConfiguration[] {
     return this.config.components;
   }
 
+  /**
+  * Registers a component that can be injected as a tab.  AJM: I dislike leveraging the component, but for now I'm going for minimal change.
+  * Ideally, the service will resolve the component, however, that might have issues later on with popout MDI (multiple GL)...pros and cons
+  * @param name 	The name of the component, as referred to by componentName in the component configuration.
+  * @param name 	The name of the component, as referred to by componentName in the component configuration.
+  * @param component 	A constructor or factory function. Will be invoked with new and two arguments, a containerobject and a component state
+  */
+  public registerComponent(host: GoldenLayoutComponent, name: String, componentType: Type<any>): void {
+    const compCallback = host.createComponentInitCallback(componentType);
+    this._layout.registerComponent(name, compCallback);
+  }
+
+  /**
+   * AJM: Why createContentItem at the first slot every time, as opposed to addChild, etc.
+   * @param comp pulls 'name'...seems overly complex...
+   * @param title tab title
+   * @param state componentState
+   */
   public createNewComponent(comp: ComponentConfiguration, title?: string, state?: any) {
     // create content item
     const content = this._layout.createContentItem({
@@ -77,7 +104,7 @@ export class GoldenLayoutService {
       element = this.findStack(root.contentItems);
     }
     if (element === null) {
-      throw new Error("this should never happen!");
+      throw new Error('this should never happen!');
     }
     element.addChild(content);
   }
--- projects/ee-golden-layout/src/lib/state.ts
+++ projects/ee-golden-layout/src/lib/state.ts
@@ -3,9 +3,9 @@ import { InjectionToken, Provider } from '@angular/core';
 export const GoldenLayoutStateStore = new InjectionToken('GoldenLayoutStateStore');
 
 export interface StateStore {
-  writeState(state:any): void;
+  writeState(state: any): void;
 
-  loadState(): Promise<any>
+  loadState(): Promise<any>;
 }
 
 export const DEFAULT_LOCAL_STORAGE_STATE_STORE_KEY = '$ng-golden-layout-state';
@@ -13,14 +13,14 @@ export const DEFAULT_LOCAL_STORAGE_STATE_STORE_KEY = '$ng-golden-layout-state';
 export class LocalStorageStateStore implements StateStore {
   constructor(private readonly key: string) {}
 
-  public writeState(state: any): void { 
+  public writeState(state: any): void {
     localStorage.setItem(this.key, JSON.stringify(state));
   }
 
   public loadState(): Promise<any> {
     const state = localStorage.getItem( this.key );
-    return state 
-      ? Promise.resolve(JSON.parse(state)) 
+    return state
+      ? Promise.resolve(JSON.parse(state))
       : Promise.reject(`No state found using key: ${this.key}`);
   }
 }
