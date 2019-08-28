92731c9b45e69fb66df4f8ce70374202de3bd7eb
Anthony Mandra <anthony@C02Z12HJLVDQ.local>
2019-08-28 15:03:57 -0400

    ivy overlay test

--- projects/panel-library/src/lib/panel-library.component.ts
+++ projects/panel-library/src/lib/panel-library.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-panel-library',
-  template: `
-    <p>
-      Hello from the dynamically loaded plugin!
-    </p>
-  `,
+  templateUrl: `panel-library.component.html`,
   styles: []
 })
 export class PanelLibraryComponent implements OnInit {
--- projects/panel-library/src/lib/panel-library.module.ts
+++ projects/panel-library/src/lib/panel-library.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PanelLibraryComponent } from './panel-library.component';
 import { forChild } from 'ngx-golden-layout';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const TYPES = [{
   name: 'plugin-lib',
@@ -8,6 +11,11 @@ const TYPES = [{
 }];
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatTooltipModule
+  ],
   declarations: [PanelLibraryComponent],
   providers: [...forChild(TYPES)],
   exports: [PanelLibraryComponent],
